8cc68db7ddf85d0cee9d213f6c7686625278a678
BluamekGD <bartosz.nicely@gmail.com>
2026-09-06 14:14:32 +0200

    hi everyone

---
+++ ROADMAP.md
@@ -0,0 +1,16 @@
+# pretty ls
+
+A pretty ls replacement **made in Rust**, inspired by the nushell ls.
+
+## Roadmap:
+
++ \[in progress\] Make sure every single soul using this knows its written in rust
++ \[done\] Make it actually work
++ \[done\] Make it a table
++ \[done\] Add -a
++ \[done\] Add -g
++ Sorting
++ Icons
++ Colors
++ Add --long
+
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,48 @@
 use std::fs;
-use std::env;
 use std::path::PathBuf;
 use comfy_table::*;
-use comfy_table::presets::UTF8_FULL;
+use comfy_table::presets::UTF8_FULL_CONDENSED;
+use clap::Parser;
+use ignored::is_ignored;
+
+#[derive(Parser, Debug)]
+#[command(about, version)]
+
+struct Args {
+    #[arg(default_value = ".")]
+    path: PathBuf,
+
+    #[arg(short, long, help = "Show hidden files")]
+    all: bool,
+
+    #[arg(short, long, help = "Hide files ignored by Git")]
+    gitignore: bool,
+}
 
 fn main() -> std::io::Result<()> {
 
-    let args: Vec<String> = env::args().skip(1).collect();
-    let path_str = args.first().map(|s| s.as_str()).unwrap_or(".");
-    let directory = PathBuf::from(path_str);
+    let args = Args::parse();
+    let directory = args.path;
 
     let mut files = Vec::new();
 
     for entry in fs::read_dir(&directory)? {
         let entry = entry?;
-        files.push(entry.file_name().to_string_lossy().into_owned());
+        let fname = entry.file_name().to_string_lossy().into_owned();
+        let fpath = entry.path();
+
+        if !args.all && fname.starts_with(".") {
+            continue;
+        }
+        if args.gitignore && is_ignored!(&fpath) {
+            continue;
+        }
+        files.push(fname.clone());
     }
 
     let mut table = Table::new();
     table
-        .load_style(UTF8_FULL.with_rounded_corners())
+        .load_style(UTF8_FULL_CONDENSED.with_rounded_corners())
         .set_header(vec!["#", "File", "#"]);
 
     for (i, file) in files.iter().enumerate() {
