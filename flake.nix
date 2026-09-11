9f2e1df727b299b2c2313609b52d23bfc3c85d90
BluamekGD <bartosz.nicely@gmail.com>
2026-09-11 20:08:14 +0200

    my beloved nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A pretty ls replacement, inspired by the nushell ls.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "pretty-ls";
+          version = "0.1.0";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/pls";
+        };
+      }
+    );
+}
